f621a4cdd34c0e7f20a6e7bf46f0f891675c2610
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-01-23 21:38:49 +0000

    feat(liked-page): add no liked dogs message
    Display a message when there are no liked dogs in the liked page.
    This improves the user experience by providing feedback when the
    list is empty.

--- app/liked/page.tsx
+++ app/liked/page.tsx
@@ -45,11 +45,13 @@ export default async function LikePage() {
 		<div className="container mx-auto px-4 md:px-6 lg:px-8">
 			<h1 className="text-2xl font-bold mt-8">Liked Dogs</h1>
 			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-8">
-				{dogListUrls.map(url => (
-					<Suspense key={url} fallback={<div>Loading...</div>}>
-						<LikedDogCard url={url} />
-					</Suspense>
-				))}
+				{dogListUrls.length > 0
+					? dogListUrls.map(url => (
+							<Suspense key={url} fallback={<div>Loading...</div>}>
+								<LikedDogCard url={url} />
+							</Suspense>
+						))
+					: <div>No liked dogs</div>}
 			</div>
 		</div>
 	);
